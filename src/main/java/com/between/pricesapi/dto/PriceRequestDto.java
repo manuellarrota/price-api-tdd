49956696f9d9eb366093181b3087918b89091a0e
manuel <lenguaje100natural@gmail.com>
2023-04-23 22:48:44 -0300

    - Se agregan atributos a PriceRequestDto y PriceResponseDto. - Se agregan notaciones NoArgsConstructor y Data, a PriceRequestDto y PriceResponseDto.

--- src/main/java/com/between/pricesapi/dto/PriceRequestDto.java
+++ src/main/java/com/between/pricesapi/dto/PriceRequestDto.java
@@ -1,4 +1,14 @@
 package com.between.pricesapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PriceRequestDto {
+    private String applicationDate;
+    private Long productId;
+    private Integer brandId;
 }
--- src/main/java/com/between/pricesapi/dto/PriceResponseDto.java
+++ src/main/java/com/between/pricesapi/dto/PriceResponseDto.java
@@ -1,4 +1,23 @@
 package com.between.pricesapi.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@NoArgsConstructor
+@Data
 public class PriceResponseDto {
+    private Long productId;
+    private Long priceId;
+    private Double price;
+    private Integer brandId;
+    private Date applicationDate;
+
+    public PriceResponseDto(Long productId, Long priceId, Double price, Integer brandId, Date applicationDate) {
+        this.productId = productId;
+        this.brandId = brandId;
+        this.priceId = priceId;
+        this.price = price;
+        this.applicationDate = applicationDate;
+    }
 }
