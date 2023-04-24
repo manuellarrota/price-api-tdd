eb54c9cf31c33689ea976576c613219f6323887c
manuel <lenguaje100natural@gmail.com>
2023-04-24 02:14:45 -0300

    - Se implementa el servicio

--- src/main/java/com/between/pricesapi/service/PriceServiceImpl.java
+++ src/main/java/com/between/pricesapi/service/PriceServiceImpl.java
@@ -1,13 +1,22 @@
 package com.between.pricesapi.service;
 
 import com.between.pricesapi.model.Price;
+import com.between.pricesapi.repository.PriceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 @Service
 public class PriceServiceImpl implements PriceService {
+    private final PriceRepository priceRepository;
+
+    @Autowired
+    public PriceServiceImpl(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
+    }
+
     @Override
     public Price getPriceByDateAndBrandAndProduct(Date applicationDate, Long productId, Integer brandId) {
-        return null;
+        return priceRepository.getPriceByDateAndBrandAndProduct(applicationDate, productId, brandId);
     }
 }
