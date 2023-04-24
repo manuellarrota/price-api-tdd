cb49d6ef8cbe44be9efd8a63abd89df51c7250bb
manuel <lenguaje100natural@gmail.com>
2023-04-24 02:15:18 -0300

    - Se implementa el repositorio

--- src/main/java/com/between/pricesapi/repository/PriceRepository.java
+++ src/main/java/com/between/pricesapi/repository/PriceRepository.java
@@ -1,13 +1,22 @@
 package com.between.pricesapi.repository;
 
 import com.between.pricesapi.model.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 
 @Repository
-public interface PriceRepository {
+public interface PriceRepository extends JpaRepository<Price, Long> {
+    @Query(nativeQuery = true,
+            value = "Select p.* from price p " +
+                    " where :applicationDate between p.start_date and p.end_date" +
+                    " and p.product_id = :productId" +
+                    " and p.brand_id = :brandId" +
+                    " order by priority desc" +
+                    " limit 1")
     Price getPriceByDateAndBrandAndProduct(
             @Param("applicationDate") Date applicationDate,
             @Param("productId") Long productId,
