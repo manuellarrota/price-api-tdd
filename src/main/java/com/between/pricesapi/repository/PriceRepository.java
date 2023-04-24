e0de631dc719221ceb7ecc2acf5442e1a4e0269e
manuel <lenguaje100natural@gmail.com>
2023-04-23 22:11:59 -0300

    - Se agrega notacion Repository a PriceRepository - Se crea la firma con los parametros de findPriceByDateAndBrandAndProduct, que retornara el precio del producto deseado

--- src/main/java/com/between/pricesapi/repository/PriceRepository.java
+++ src/main/java/com/between/pricesapi/repository/PriceRepository.java
@@ -1,4 +1,15 @@
 package com.between.pricesapi.repository;
 
+import com.between.pricesapi.model.Price;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
 public interface PriceRepository {
+    Price findPriceByDateAndBrandAndProduct(
+            @Param("applicationDate") Date applicationDate,
+            @Param("productId") Long productId,
+            @Param("brandId") Integer brandId);
 }
